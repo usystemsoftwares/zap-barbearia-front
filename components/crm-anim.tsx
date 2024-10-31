293ba1d7d08a0ce2f79feeb7a2b16bf728ab776b
Raphael Martinez <raphael.martinez@usystem.com.br>
2024-10-30 22:41:13 -0300

    style: chane texts

--- components/crm-anim.tsx
+++ components/crm-anim.tsx
@@ -11,14 +11,12 @@ import FadeUp from "./fade-up";
 
 const CRMAnim = () => {
   return (
-    <motion.ul
-      className="grid w-[min(100%,35rem)] mx-auto leading-[1.2] gap-6 lg:gap-3 lg:mx-0 lg:w-auto lg:grid-cols-[repeat(30,minmax(0,1fr))] *:flex *:flex-col *:rounded-[1.2rem] *:px-[.4rem] *:pt-4 *:pb-2 *:overflow-hidden *:bg-white *:shadow-md text-start relative [&_p]:pt-2 [&_p]:pb-4 [&_p]:mx-4 [&_p]:leading-[1.1] [&_h3]:mx-4"
-    >
+    <motion.ul className="grid w-[min(100%,35rem)] mx-auto leading-[1.2] gap-6 lg:gap-3 lg:mx-0 lg:w-auto lg:grid-cols-[repeat(30,minmax(0,1fr))] *:flex *:flex-col *:rounded-[1.2rem] *:px-[.4rem] *:pt-4 *:pb-2 *:overflow-hidden *:bg-white *:shadow-md text-start relative [&_p]:pt-2 [&_p]:pb-4 [&_p]:mx-4 [&_p]:leading-[1.1] [&_h3]:mx-4">
       <FadeUp className="lg:[grid-area:span_1/span_15]">
         <h3 className="text-3xl font-semibold">Agenda</h3>
         <p className="">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos tenetur,
-          omnis exercitationem
+          Controle completo de agendamentos no CRM ou WhatsApp: gerencie
+          serviços, barbeiros, tipos e bloqueio de horários com facilidade!
         </p>
         <div className="bg-gradient-to-tr py-6 lg:py-8 flex-1 grid place-items-center rounded-[.5rem] to-[#cdfbcd] from-[#9ef1ce]">
           <div className="overflow-hidden w-[90%] lg:w-4/5 rounded-[.4rem]">
@@ -31,10 +29,10 @@ const CRMAnim = () => {
         </div>
       </FadeUp>
       <FadeUp className="lg:[grid-area:span_1/span_15]">
-        <h3 className="text-3xl font-semibold">Conexão com Whatsapp</h3>
+        <h3 className="text-3xl font-semibold">Atendente de Whatsapp</h3>
         <p className="">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos tenetur,
-          omnis exercitationem
+          atendente inteligente e personalizado, com tom ajustável e aprendizado
+          contínuo sobre sua barbearia!
         </p>
         <div className="bg-gradient-to-tr py-6 lg:py-8 flex-1 grid place-items-center rounded-[.5rem] to-[#94ebe8] from-[#b1f0e8]">
           <div className="overflow-hidden w-[90%] lg:w-4/5 rounded-[.4rem]">
@@ -45,8 +43,7 @@ const CRMAnim = () => {
       <FadeUp className="lg:[grid-area:span_1/span_10]">
         <h3 className="text-3xl font-semibold">Painel</h3>
         <p className="">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos tenetur,
-          omnis exercitationem
+          Visualize agendamentos, faturamento e desempenho da barbearia!
         </p>
         <div className="bg-gradient-to-tr py-6 flex-1 grid place-items-center rounded-[.5rem] to-[#fce1d4] from-[#fff3c3]">
           <div className="overflow-hidden w-[90%] rounded-[.4rem]">
@@ -61,8 +58,7 @@ const CRMAnim = () => {
       <FadeUp className="lg:[grid-area:span_1/span_10]">
         <h3 className="text-3xl font-semibold">Tabela de clientes</h3>
         <p className="">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos tenetur,
-          omnis exercitationem
+          Gestão completa de clientes que entram em contato pelo WhatsApp!
         </p>
         <div className="bg-gradient-to-tr py-6 flex-1 grid place-items-center rounded-[.5rem] to-[#d6ddf9] from-[#eedbfa]">
           <div className="overflow-hidden w-[90%] rounded-[.4rem]">
@@ -77,8 +73,7 @@ const CRMAnim = () => {
       <FadeUp className="lg:[grid-area:span_1/span_10]">
         <h3 className="text-3xl font-semibold">Minha Barbearia</h3>
         <p className="">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos tenetur,
-          omnis exercitationem
+          Controle horários, funcionários, serviços e pacotes com facilidade.
         </p>
         <div className="bg-gradient-to-tr py-6 flex-1 grid place-items-center rounded-[.5rem] to-[#fdd8eb] from-[#fee5ff]">
           <div className="overflow-hidden w-[90%] rounded-[.4rem]">
